068a7303e30b08d6eaf86671ec9e11222809ceca
kingsiva123 <arulsiva.sjob@gmail.com>
2022-03-13 16:15:19 +0530

    singleton

---
+++ src/org/test/Singleton.java
@@ -0,0 +1,34 @@
+package org.test;
+
+
+
+public class Singleton {
+	static Singleton in;
+	private static Singleton add() {
+		if (in==null) {
+			System.out.println(System.identityHashCode(in));
+			
+			in = new Singleton();
+			System.out.println(System.identityHashCode(in));
+			
+		}
+
+		return in;
+		
+
+	}
+	public static void main(String[] args) {
+		Singleton add = in.add();
+		System.out.println(System.identityHashCode(add));
+		
+		Singleton add2 = in.add();
+		System.out.println(System.identityHashCode(add2));
+	}
+		
+		
+	
+
+}
+
+
+
